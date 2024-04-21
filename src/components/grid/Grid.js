abe451d9778797158e98fe75c578be821f5473c1
cloud-spotter <cloudspotter0@gmail.com>
2024-04-21 17:35:50 +0100

    First Grid test passing. Could do with some CSS applied to components now to visualise.

--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -1,8 +1,19 @@
+import React from 'react';
 import Card from '../card/Card';
 
 function Grid() {
-    // TODO: implement code to pass Grid test
-    return <div><Card /></div>; 
+    const totalValues = 16; // Start with a fixed grid for testing & first iteration
+    const createPairSequence = (total) => {
+        const sequence = [...Array(Math.floor(total / 2)).keys()].map(i => i + 1);
+        return [...sequence, ...sequence]; // Create a paired sequence of values
+    };
+    
+    const cardValues = createPairSequence(totalValues);
+
+    const cards = cardValues.map((value, index) => (
+        <Card key={index} value={value} />
+    ));
+    return <div className="grid">{cards}</div>; 
 }
 
 export default Grid;
