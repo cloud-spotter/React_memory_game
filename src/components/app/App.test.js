00cd2da3b52b1940116d6e04452fe9b1347d0fc1
cloud-spotter <cloudspotter0@gmail.com>
2024-05-18 11:46:27 -0700

    Fixed broken App test. Refactored test to check that the title is present, agnostic of how it's structured in the HTML (test had failed when I split the header title over two lines)

--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -1,12 +1,19 @@
 // tests/integration/App.test.js
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
   test('renders header with game title', () => {
     render(<App />);
-    const gameTitle = screen.getByText(/Memory Game/i);
+    
+    // Check that the title features, agnostic of how it's structured in the HTML
+    const headerContainer = screen.getByRole('heading'); // Get the header container   
+    // Use within to scope the query to the header container
+    const gameTitle = within(headerContainer).getByText((content, element) => {
+      return /Memory\s*Game/i.test(element.textContent); // Regex matching to check for title in case insensitive way ('i' flag), allowing for any whitespace ('\s*')
+    });
+
     expect(gameTitle).toBeInTheDocument();
   });
 
