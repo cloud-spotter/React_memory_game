7d26747f33d7a86847c13822db42ebe00f58b4f2
cloud-spotter <cloudspotter0@gmail.com>
2024-05-21 13:19:56 -0700

    Add styling to the GameOverModal component and a transparency overlay for the background, conditionally rendered when the modal is open

--- src/components/game-over-modal/GameOverModal.js
+++ src/components/game-over-modal/GameOverModal.js
@@ -3,11 +3,18 @@ import Modal from 'react-modal';
 
 function GameOverModal({ isOpen, closeModal, moveCount, startGame }) {
     return (
-      <Modal isOpen={isOpen} onRequestClose={closeModal} contentLabel='Game Over'>
-        <h2>GameOver!</h2>
-        <div>You found all the pairs in {moveCount} moves</div>
-        <button onClick={startGame}>Play Again</button>
-      </Modal>
+      <>
+        {isOpen && (
+          <div className='modal-overlay'>
+            <Modal className={"game-over-modal"} overlayClassName={'modal-overlay'} isOpen={isOpen} onRequestClose={closeModal} contentLabel='Game Over'>
+              <h2>Game Over!</h2>
+              <div>You found all the pairs in {moveCount} moves</div>
+              <br></br>
+              <button className='button play-again' aria-label='Play Again' onClick={startGame}>Play Again</button>
+            </Modal>
+          </div>
+        )}
+      </>
     );
 }
 
