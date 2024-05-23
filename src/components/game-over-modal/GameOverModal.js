cff2c9dba2bcefce3d741319209189459ae844e5
cloud-spotter <cloudspotter0@gmail.com>
2024-05-23 12:22:32 -0700

    Refactored the onRequestClose handler for the GameOverModal, to include a game reset, so that the timer doesn't keep running if a user closes the modal by clicking outside it

--- src/components/game-over-modal/GameOverModal.js
+++ src/components/game-over-modal/GameOverModal.js
@@ -2,11 +2,17 @@ import React from 'react';
 import Modal from 'react-modal';
 
 function GameOverModal({ isOpen, closeModal, moveCount, startGame, resetGame, timer }) {
+    // Reset game (including stopping the timer) when the modal is closed
+    const handleRequestClose = () => {
+        resetGame(); 
+        closeModal(); 
+    };
+
     return (
       <>
         {isOpen && (
           <div className='modal-overlay'>
-            <Modal className={"game-over-modal"} overlayClassName={'modal-overlay'} isOpen={isOpen} onRequestClose={closeModal} contentLabel='Game Over'>
+            <Modal className={"game-over-modal"} overlayClassName={'modal-overlay'} isOpen={isOpen} onRequestClose={handleRequestClose} contentLabel='Game Over'>
               <h2>Game Over!</h2>
               <div>You found all the pairs in {moveCount} moves.</div>
               <div>Time: {timer} seconds</div>
