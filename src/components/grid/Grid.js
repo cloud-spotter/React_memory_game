188b740b79309c1c8e58a5e99cfe65353f4cdbe4
cloud-spotter <cloudspotter0@gmail.com>
2024-06-23 13:50:47 +0100

    Adapt Grid component to take columns and rows props and set these up to be used in new gridStyle object (to be passed to CSS style attribute) to dynamically set the grid size from the user's selection in Home.js

--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Card from '../card/Card';
 
-function Grid({ cards, handleCardClick }) {
+function Grid({ columns, rows, cards, handleCardClick }) {
+    // New object to pass to CSS style attribute, to set grid columns/rows dynamically from user choice in Home.js
+    const gridStyle = {  
+        display: 'grid',
+        gridTemplateColumns: `repeat(${columns}, 1fr)`,
+        gridTemplateRows: `repeate(${rows}, 1fr)`
+    };
+    
     const cardElements = cards.map((card, index) => (
         <Card key={index} card={card} handleCardClick={() => handleCardClick(index)} />
     ));
 
-    return <div className="grid" data-testid="grid">{cardElements}</div>; 
+    return <div className="grid" style={gridStyle} data-testid="grid">{cardElements}</div>; 
 };
 
 export default Grid;
