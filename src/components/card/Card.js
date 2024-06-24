b7c2d307bc56e295452b10f824a711fa07114963
cloud-spotter <cloudspotter0@gmail.com>
2024-06-24 18:43:39 +0100

    Create wrapper for card images to ensure they're all displayed at a consistent size.

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,14 +3,16 @@ import React from "react";
 function Card({ card, handleCardClick }) {
     return (
       <button
-        // Use template string to construct className string (to combine multiple classes conditionally)
         className={`${card.isFlipped ? "card-faceup" : "card-facedown"} ${card.isMatched ? "card-matched" : ""}`}
-        // aria-label added to enhance accessibility
         aria-label={card.isFlipped ? `Card faceup with image` : "Card facedown"} 
         onClick={handleCardClick}
         data-value={card.image}
       >
-        {card.isFlipped ? <img src={card.image} alt="card" style={{ width: '70%', height: '70%' }} /> : ""}
+        {card.isFlipped && (
+          <div className="card-image-wrapper">
+            <img src={card.image} alt="card" />
+          </div>
+        )}
       </button>
     )
 }
