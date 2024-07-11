f610e003590a19b97ca4ef805d09154b58a5cbc4
cloud-spotter <cloudspotter0@gmail.com>
2024-07-10 17:28:22 -0700

    Update Card component to include getAriaLabel() function which updates aria-label with the new card image description when flipped or matched, or the new card id if the card is facedown. Two Card unit tests used the old aria-label (Card faceup with image) which will break now and so will need updating also.

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,20 +1,31 @@
 import React from "react";
 
 function Card({ card, handleCardClick }) {
-    return (
+  // Set card aria-labels for accessibility
+  const getAriaLabel = () => {
+    if (card.isMatched) {
+        return `Matched card: ${card.description}`;
+    } else if (card.isFlipped) {
+        return `Flipped card: ${card.description}`;
+    } else {
+        return `Card ${card.id} facedown`;
+    }
+  };
+  
+  return (
       <button
         className={`${card.isFlipped ? "card-faceup" : "card-facedown"} ${card.isMatched ? "card-matched" : ""}`}
-        aria-label={card.isFlipped ? `Card faceup with image` : "Card facedown"} 
+        aria-label={getAriaLabel()} 
         onClick={handleCardClick}
         data-value={card.image}
       >
         {card.isFlipped && (
           <div className="card-image-wrapper">
-            <img src={card.image} alt="card" />
+            <img src={card.image} alt={card.description} />
           </div>
         )}
       </button>
-    )
+    );
 }
 
 export default Card;
